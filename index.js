f14f1714d8c9f982a5c3c02c039ad2d0628de454
niaz-morshed-m <niazmorshed14007@gmail.com>
2025-12-12 00:12:14 +0600

    api for creating checkout session for strip payment initialised

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
@@ -33,7 +34,7 @@ async function run() {
       const cursor = bookCollection.find();
 
       const result = await cursor.toArray();
-    
+
       res.send(result);
     });
 
@@ -43,67 +44,106 @@ async function run() {
         .sort({
           addedAt: -1,
         })
-        .limit(6);;
+        .limit(6);
 
       const result = await cursor.toArray();
-    
+
       res.send(result);
     });
 
-
- app.get("/book/details/:id", async (req, res) => {
+    app.get("/book/details/:id", async (req, res) => {
       const id = req.params.id;
-    let query;
-    if (ObjectId.isValid(id)) {
-      query = {
-        $or: [{ _id: new ObjectId(id) }, { _id: id }],
-      };
-    } else {
-      query = { _id: id };
-    }
+      let query;
+      if (ObjectId.isValid(id)) {
+        query = {
+          $or: [{ _id: new ObjectId(id) }, { _id: id }],
+        };
+      } else {
+        query = { _id: id };
+      }
       const result = await bookCollection.findOne(query);
       res.send(result);
     });
 
+    app.post("/order", async (req, res) => {
+      const newOrder = req.body;
+      newOrder.createdAt = new Date();
+      const result = await orderCollection.insertOne(newOrder);
+      res.send(result);
+    });
 
-    
+    app.get("/order/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: email,
+      };
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/order/id/:id", async (req, res) => {
+      const id = req.params.id;
+      let query;
+      if (ObjectId.isValid(id)) {
+        query = {
+          $or: [{ _id: new ObjectId(id) }, { _id: id }],
+        };
+      } else {
+        query = { _id: id };
+      }
+      const result = await orderCollection.findOne(query);
 
- app.post("/order", async (req, res) => {
-   const newOrder = req.body;
-newOrder.createdAt= new Date()
-   const result = await orderCollection.insertOne(newOrder);
-   res.send(result);
- });
+      res.send(result);
+    });
 
+    app.patch("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedStatus = req.body;
+      let query;
+      if (ObjectId.isValid(id)) {
+        query = { _id: new ObjectId(id) };
+      } else {
+        query = { _id: id };
+      }
 
- app.get("/order/:email", async (req, res) => {
-   const email = req.params.email;
-   const query = {
-    email : email
-   }
+      const update = { $set: { status: updatedStatus.status } };
 
-app.patch("/order/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedStatus = req.body;
-  let query;
-  if (ObjectId.isValid(id)) {
-    query = { _id: new ObjectId(id) };
-  } else {
-    query = { _id: id };
-  }
+      const result = await orderCollection.updateOne(query, update);
+      res.send(result);
+    });
 
-  const update = { $set: { status: updatedStatus.status } };
+    app.post('/create-checkout-session', async (req, res) => {
+        const paymentInfo = req.body;
+        const amount = parseInt(paymentInfo.cost) * 100;
 
-  const result = await orderCollection.updateOne(query, update);
-  res.send(result);
-});
+        const session = await stripe.checkout.sessions.create({
+          line_items: [
+            {
+              price_data: {
+                currency: "USD",
+                unit_amount: amount,
+                product_data: {
+                  name: paymentInfo.bookName,
+                },
+              },
+              quantity: 1,
+            },
+          ],
+          customer_email: paymentInfo.email,
+          mode: "payment",
+          metadata: {
+            parcelId: paymentInfo._id,
+            parcelName: paymentInfo.bookName,
+          },
+          success_url: `${process.env.SITE_URL}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${process.env.SITE_URL}/dashboard/payment-cancelled`,
+        });
 
-   const cursor =  orderCollection.find(query)
-   const result = await cursor.toArray();
-   res.send(result);
- });
-    
+        console.log(session)
+        res.send({ url: session.url })
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -114,12 +154,13 @@ app.patch("/order/:id", async (req, res) => {
     // await client.close();
   }
 }
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
